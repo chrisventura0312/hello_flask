5f466fa45392802ce5cf6a1047f875feac3028a8
Chris Ventura <cvent820@gmail.com>
2023-05-21 21:45:11 -0700

    inital commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:name>')
+def say(name):
+    return f"Hi {name}!"
+
+@app.route('/repeat/<int:num>/<string:word>')
+def repeat(num, word):
+    return f"{word} " * num
+
+@app.route('/<path>')
+def catch_all(path):
+    return "Sorry! No response. Try again."
+
+if __name__ == "__main__":
+    app.run(debug=True)
